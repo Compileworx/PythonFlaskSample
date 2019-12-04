56bab8dbeb21183f967389f485ba2f514e499d5e
bradleycunningham <bradleycunningham@penguin>
2019-12-04 20:34:27 +1100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "helloThere.html",
+        name=name,
+        date=datetime.now()
+    )
